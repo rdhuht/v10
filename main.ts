757dfd07f6d21f9ad9bde689c7b400777519ebee
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2020-12-18 16:13:15 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,35 @@
+input.onButtonPressed(Button.A, function () {
+    game.addScore(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    game.setScore(0)
+})
+input.onButtonPressed(Button.B, function () {
+    game.addScore(-1)
+})
+let tm = TM1637.create(
+DigitalPin.P1,
+DigitalPin.P2,
+7,
+4
+)
+pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
+music.setVolume(125)
+music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+game.startCountdown(10000)
+game.setScore(0)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P8) == 1) {
+        music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+        basic.showLeds(`
+            # # # # #
+            # # . # #
+            # . # . #
+            # # . # #
+            # # # # #
+            `)
+        game.addScore(1)
+    }
+    tm.showNumber(game.score())
+    basic.showNumber(game.score())
+})
